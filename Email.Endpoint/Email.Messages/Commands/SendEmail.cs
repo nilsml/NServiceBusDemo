550e1f7b577cc64df970e4630be9a9a9edbc9780
Nils Magne Lunde <nilsml@nilsland.com>
2013-03-22 18:16:47 +0100

    Added messages project

---
+++ Email.Endpoint/Email.Messages/Commands/SendEmail.cs
@@ -0,0 +1,16 @@
+﻿namespace Email.Messages.Commands
+{
+    public class SendEmail
+    {
+        public string To { get; private set; }
+        public string Subject { get; private set; }
+        public string Body { get; private set; }
+
+        public SendEmail(string to, string subject, string body)
+        {
+            To = to;
+            Subject = subject;
+            Body = body;
+        }
+    }
+}
