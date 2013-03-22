9d42fb780bfd9b418e86d9978143490415e4a328
Nils Magne Lunde <nilsml@nilsland.com>
2013-03-22 19:50:44 +0100

    Added message handler for SendEmail

---
+++ Email.Endpoint/Email.Endpoint/SendEmailHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Email.Messages.Commands;
+using NServiceBus;
+
+namespace Email.Endpoint
+{
+    public class SendEmailHandler : IHandleMessages<SendEmail>
+    {
+        public void Handle(SendEmail message)
+        {
+            Console.WriteLine(string.Format("Sending Email to: {0} with subject {1} and body\r\n{2}", message.To, message.Subject, message.Body));
+        }
+    }
+}
