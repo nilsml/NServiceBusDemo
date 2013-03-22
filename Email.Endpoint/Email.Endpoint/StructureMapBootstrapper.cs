bb8cd1d5ba7c493839b7469b0a93e01c09712f9e
Nils Magne Lunde <nilsml@nilsland.com>
2013-03-22 19:42:49 +0100

    Added IoC and installed raven client

---
+++ Email.Endpoint/Email.Endpoint/StructureMapBootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using StructureMap;
+
+namespace Email.Endpoint
+{
+    class StructureMapBootstrapper
+    {
+        public static void Bootstrap()
+        {
+            ObjectFactory.Initialize(conf => conf.Scan(scan =>
+                                                           {
+                                                               scan.TheCallingAssembly();
+                                                               scan.WithDefaultConventions();
+                                                           }));
+        }
+    }
+}
