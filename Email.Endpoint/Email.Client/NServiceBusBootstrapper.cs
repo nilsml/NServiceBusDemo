81205ec01df016f9532213d57db88a273b97ffd5
Nils Magne Lunde <nilsml@nilsland.com>
2013-03-22 21:04:23 +0100

    Added client

---
+++ Email.Endpoint/Email.Client/NServiceBusBootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using NServiceBus;
+
+namespace Email.Client
+{
+    public class NServiceBusBootstrapper
+    {
+        public static IBus Run()
+        {
+                return Configure.With()
+                                .DefineEndpointName("Email.Client")
+                                .Log4Net()
+                                .DefaultBuilder()
+                                .DefiningCommandsAs(t => t.Namespace != null && t.Namespace.Contains(".Messages") && t.Namespace.EndsWith(".Commands"))
+                                .XmlSerializer()
+                                .MsmqTransport()
+                                .IsTransactional(false)
+                                .PurgeOnStartup(false)
+                                .UnicastBus()
+                                .ImpersonateSender(false)
+                                .SendOnly();
+        } 
+    }
+}
---
+++ Email.Endpoint/Email.Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Email.Messages.Commands;
+
+namespace Email.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bus = NServiceBusBootstrapper.Run();
+            Console.WriteLine("Press a key to send a message..");
+            Console.ReadKey();
+
+            bus.Send("Email.Endpoint", new SendEmail("some.person@domain.com", "Hello there!", "This is a message body..."));
+        }
+    }
+}
