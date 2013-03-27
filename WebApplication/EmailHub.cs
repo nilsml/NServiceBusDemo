0bbc04881bdac5ce4d1cab7c57d86221bc723245
Nils Magne Lunde <nilsml@nilsland.com>
2013-03-27 21:20:24 +0100

    Added web project with default signalR hub

---
+++ WebApplication/EmailHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace WebApplication
+{
+    public class EmailHub : Hub
+    {
+        public void Send(string to, string subject, string body)
+        {
+            Clients.All.broadcastMessage(to, subject, body);
+        }
+    }
+}
