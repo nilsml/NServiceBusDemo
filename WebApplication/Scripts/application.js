c16c9f875a028ac0157c377e5fc548c18bd53d9e
Nils Magne Lunde <nilsml@nilsland.com>
2013-03-27 22:09:48 +0100

    Added very simple html and script to run the application

---
+++ WebApplication/Scripts/application.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    var init = function() {
+        var emailHub = $.connection.emailHub;
+
+        emailHub.client.broadcastMessage = function(to, subject, body) {
+            var encodedTo = $('<div />').text(to).html();
+            var encodedSubject = $('<div />').text(subject).html();
+            var encodedBody = $('<div />').text(body).html();
+
+            $('#listOfEmails').append('<li>' + encodedTo + '&nbsp;' + encodedSubject + '&nbsp;' + encodedBody + '</li>');
+        };
+
+        $.connection.hub.start();
+    };
+
+    $(document).ready(init);
+})();
