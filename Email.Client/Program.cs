716a070060f2b49eaca2517adcf98576c8e0f590
Nils Magne Lunde <nilsml@nilsland.com>
2013-03-29 19:47:26 +0100

    Showing the time in the email body and can send multiple messages

--- Email.Client/Program.cs
+++ Email.Client/Program.cs
@@ -12,10 +12,17 @@ namespace Email.Client
         static void Main(string[] args)
         {
             var bus = NServiceBusBootstrapper.Run();
-            Console.WriteLine("Press a key to send a message..");
-            Console.ReadKey();
 
-            bus.Send("Email.Endpoint", new SendEmail("some.person@domain.com", "Hello there!", "This is a message body..."));
+            do
+            {
+                Console.WriteLine("Press a key to send a message.." + Environment.NewLine + "Press Q to end the program.");
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                if (keyInfo.Key == ConsoleKey.Q)
+                    return;
+
+                bus.Send("Email.Endpoint", new SendEmail("some.person@domain.com", "Hello there!", string.Format("The time is now {0}", DateTime.Now.ToShortTimeString())));
+            } while (true);
+
         }
     }
 }
